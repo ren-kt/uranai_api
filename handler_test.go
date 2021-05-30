737c9edfd2cdb328a020c5bc0f44a2e84cfb8ac4
ren-kt <keita.takahashi@rensa.co.jp>
2021-05-31 02:29:08 +0900

    admin handerのテスト追加

--- handler_test.go
+++ handler_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,13 +16,37 @@ import (
 	"github.com/ren-kt/uranai_api/fortune"
 )
 
-type TestDB struct {
-	DB
+type TestDB struct{}
+
+func (d *TestDB) CreateTable() error {
+	return nil
 }
 
 func (d *TestDB) GetText(result string) (string, error) {
 	return "test text", nil
 }
+
+func (d *TestDB) GetFortune(id int) (*fortune.Fortune, error) {
+	return nil, nil
+}
+
+func (d *TestDB) GetFortuneAll() ([]*fortune.Fortune, error) {
+	return nil, nil
+}
+
+func (d *TestDB) Updatefortune(f *fortune.Fortune) error {
+	return nil
+}
+
+func (d *TestDB) Deletefortune(id int) error {
+	return nil
+}
+
+func (d *TestDB) Newfortune(fortune *fortune.Fortune) error {
+	return nil
+}
+
+var _ DB = &TestDB{}
 
 type RoundTripFunc func(req *http.Request) *http.Response
 
@@ -167,6 +192,183 @@ func TestApiHandler(t *testing.T) {
 
 			if s := string(b); s != tt.expected {
 				t.Errorf("unexpected response: %s", s)
+			}
+		})
+	}
+}
+
+func TestAdminIndexHandler(t *testing.T) {
+	cases := map[string]struct {
+		statusCode int
+	}{
+		"success": {statusCode: http.StatusOK},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			td := &TestDB{}
+			hs := NewHandlers(td, nil)
+			ts := httptest.NewServer(http.HandlerFunc(hs.AdminIndexHandler))
+			defer ts.Close()
+
+			resp, err := http.Get(ts.URL)
+			if err != nil {
+				t.Errorf("unexpected error %s", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+		})
+	}
+}
+
+func TestAdminCreateHandler(t *testing.T) {
+	cases := map[string]struct {
+		result     string
+		text       string
+		statusCode int
+	}{
+		"success":                             {result: "大吉", text: "test text", statusCode: http.StatusOK},
+		"error with missing result parameter": {result: "", text: "test text", statusCode: http.StatusBadRequest},
+		"error with missing text parameter":   {result: "大吉", text: "", statusCode: http.StatusBadRequest},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			td := &TestDB{}
+			hs := NewHandlers(td, nil)
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path == "/admin" {
+					hs.AdminIndexHandler(w, r)
+				} else {
+					hs.AdminCreateHandler(w, r)
+				}
+			}))
+			defer ts.Close()
+
+			v := url.Values{"result": {tt.result}, "text": {tt.text}}
+			resp, err := http.PostForm(ts.URL, v)
+			if err != nil {
+				t.Errorf("unexpected error %s", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+		})
+	}
+}
+
+func TestAdminEditHandler(t *testing.T) {
+	cases := map[string]struct {
+		id         string
+		statusCode int
+	}{
+		"success":                       {id: "1", statusCode: http.StatusOK},
+		"error where id is a character": {id: "a", statusCode: http.StatusInternalServerError},
+		"error where id is empty":       {id: "", statusCode: http.StatusInternalServerError},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			td := &TestDB{}
+			hs := NewHandlers(td, nil)
+			ts := httptest.NewServer(http.HandlerFunc(hs.AdminEditHandler))
+			defer ts.Close()
+
+			resp, err := http.Get(fmt.Sprintf("%s%s%s", ts.URL, "/admin/edit/", tt.id))
+			if err != nil {
+				t.Errorf("unexpected error %s", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+		})
+	}
+}
+
+func TestAdminUpdateHandler(t *testing.T) {
+	cases := map[string]struct {
+		id         string
+		result     string
+		text       string
+		statusCode int
+	}{
+		"success":                             {id: "1", result: "大吉", text: "test text", statusCode: http.StatusOK},
+		"error with missing result parameter": {id: "1", result: "", text: "test text", statusCode: http.StatusBadRequest},
+		"error with missing text parameter":   {id: "1", result: "大吉", text: "", statusCode: http.StatusBadRequest},
+		"error where id is a character":       {id: "a", result: "大吉", text: "test text", statusCode: http.StatusInternalServerError},
+		"error where id is empty":             {id: "", result: "大吉", text: "test text", statusCode: http.StatusInternalServerError},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			td := &TestDB{}
+			hs := NewHandlers(td, nil)
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path == fmt.Sprintf("%s%s", "/admin/edit/", tt.id) {
+					hs.AdminEditHandler(w, r)
+				} else {
+					hs.AdminUpdateHandler(w, r)
+				}
+			}))
+			defer ts.Close()
+
+			v := url.Values{"result": {tt.result}, "text": {tt.text}}
+			resp, err := http.PostForm(fmt.Sprintf("%s%s%s", ts.URL, "/admin/update/", tt.id), v)
+			if err != nil {
+				t.Errorf("unexpected error %s", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code: %d", resp.StatusCode)
+			}
+		})
+	}
+}
+
+func TestAdminDeleteHandler(t *testing.T) {
+	cases := map[string]struct {
+		id         string
+		statusCode int
+	}{
+		"success":                             {id: "1", statusCode: http.StatusOK},
+		"error with missing result parameter": {id: "a", statusCode: http.StatusInternalServerError},
+		"error with missing text parameter":   {id: "", statusCode: http.StatusInternalServerError},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			td := &TestDB{}
+			hs := NewHandlers(td, nil)
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path == "/admin" {
+					hs.AdminIndexHandler(w, r)
+				} else {
+					hs.AdminDeleteHandler(w, r)
+				}
+			}))
+			defer ts.Close()
+
+			resp, err := http.Get(fmt.Sprintf("%s%s%s", ts.URL, "/admin/delete/", tt.id))
+			if err != nil {
+				t.Errorf("unexpected error %s", err)
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != tt.statusCode {
+				t.Errorf("unexpected status code: %d", resp.StatusCode)
 			}
 		})
 	}
